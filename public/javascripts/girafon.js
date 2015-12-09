100026a4163ea69378c10f8c66d6c858c837a881
JLoroy <justin@loroy.be>
2015-12-09 01:10:20 +0100

    header http+s

--- public/javascripts/girafon.js
+++ public/javascripts/girafon.js
@@ -4,5 +4,5 @@
 var app = angular.module('girafon',[]);
 
 app.controller('girafonController', ['$scope', '$filter', '$http','$window', function($scope, $filter, $http, $window) {
-
+$scope.test = "it works ! ";
 }]);
