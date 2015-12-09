de37b8ffe28d9c5052fe3524e3227b04ef5dc5a3
JLoroy <justin@loroy.be>
2015-12-09 01:04:25 +0100

    start js

--- public/javascripts/girafon.js
+++ public/javascripts/girafon.js
@@ -1,3 +1,8 @@
 /**
  * Created by Justin on 09-12-15.
  */
+var app = angular.module('girafon',[]);
+
+app.controller('girafonController', ['$scope', '$filter', '$http','$window', function($scope, $filter, $http, $window) {
+
+}]);
