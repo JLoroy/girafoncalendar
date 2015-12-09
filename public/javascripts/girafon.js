50236cfe09a82f69e7ada83e044e9dabad83ec29
fsouris <souris.fabian@gmail.com>
2015-12-09 01:10:01 +0100

    html header fixes

--- public/javascripts/girafon.js
+++ public/javascripts/girafon.js
@@ -1,6 +1,4 @@
-/**
- * Created by Justin on 09-12-15.
- */
+
 var app = angular.module('girafon',[]);
 
 app.controller('girafonController', ['$scope', '$filter', '$http','$window', function($scope, $filter, $http, $window) {
