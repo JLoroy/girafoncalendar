404d798dc557f6a88559c2401a00f321954063cc
JLoroy <justin@loroy.be>
2015-12-09 01:40:40 +0100

    table debut

--- public/javascripts/girafon.js
+++ public/javascripts/girafon.js
@@ -3,4 +3,9 @@ var app = angular.module('girafon',[]);
 
 app.controller('girafonController', ['$scope', '$filter', '$http','$window', function($scope, $filter, $http, $window) {
 $scope.test = "it works ! ";
+    $scope.users = {
+    1:{name:"justin"},
+    2:{name:"fab"},
+    3:{name:"girafon"}
+    };
 }]);
